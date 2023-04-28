a6a6116bff6ba5dcb315a7543a3398f102d27d2a
Phill Conrad <phtcon@ucsb.edu>
2023-04-28 12:48:21 -0700

    pc - add logging for hello controller

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -2,12 +2,15 @@ package edu.ucsb.cs156.spring.hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
+@Log4j2
 public class HelloController {
     
     @RequestMapping("/")
     public String index() {
+        log.info("HelloController.index() called");
         return "Greetings from Spring Boot!";
     }
     
